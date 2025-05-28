ec29f9624d370130244d82125f95e687fb8f6cda
LeqendJojo11 <161504432+LeqendJojo11@users.noreply.github.com>
2025-05-28 15:29:37 +0200

    update

---
+++ phptest.php
@@ -0,0 +1,7 @@
+<?php
+// Einfache PHP-Testdatei
+echo "<h1>PHP funktioniert!</h1>";
+echo "<p>PHP-Version: " . phpversion() . "</p>";
+echo "<p>Server-Software: " . $_SERVER['SERVER_SOFTWARE'] . "</p>";
+phpinfo();
+?>
